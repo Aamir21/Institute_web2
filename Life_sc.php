cb67f6a7dd612afc85b4a2d23582855d94bcba23
Md. Aamir Sohail <42406623+Aamir21@users.noreply.github.com>
2021-03-05 19:55:54 +0530

    Add files via upload

---
+++ Life_sc.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+
+<html>
+
+    <body>
+        
+        <head>
+            
+            <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Advent+Pro|Alfa+Slab+One|Anton|Archivo+Narrow|Baloo+2|Baloo+Chettan+2|Bellota|BenchNine|Caveat|Cinzel|Comfortaa|Concert+One|Cookie|Damion|Dancing+Script|Dosis|Electrolize|Fredoka+One|Great+Vibes|Indie+Flower|Inter|Josefin+Sans|Josefin+Slab|Kaushan+Script|Lato|Lobster|Luckiest+Guy|Monoton|Montserrat|Open+Sans|Oswald|Oxanium|PT+Sans+Caption|Pacifico|Parisienne|Passion+One|Poiret+One|Poppins|Quattrocento+Sans|Raleway|Righteous|Roboto+Mono|Sacramento|Satisfy|Sen|Source+Sans+Pro|Spartan|Titillium+Web|Ubuntu|Ultra|Yellowtail&display=swap" rel="stylesheet">
+            
+            
+            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+            <meta charset="utg-8">
+    
+            <meta name="viewport" width="device-width , initial-scale=1">
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+            <!-- Popper JS -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+            
+            
+            
+            
+        
+            <style type="text/css">
+                
+                .seg1{
+                    height: 180px;
+                    background-color: beige;
+                    
+                    
+                }
+                #header{
+                    width: 280px;
+                    height: 100px;
+                    background-color: gold;
+                    margin-top: 80px;
+                    
+                }
+                
+                #header h3{
+                    
+                    font-family: Poppins;
+                    padding-top: 20px;
+                    padding-left: 60px;
+                    padding-right: 20px;
+                }
+                
+                #header a{
+                    font-size: 40px;
+                    color: black;
+                    position: absolute;
+                    margin-top: -23px;
+                    margin-left: 10px;
+                }
+                
+                .seg2{
+                    height: 600px;
+                    background-color: aliceblue;
+                }
+                
+            
+            
+            
+            
+            
+            
+            </style>
+        
+        
+        </head>
+        
+        
+        <div class="container-fluid">
+        
+            <div class="row">
+                
+                <div class="col-md-12 seg1">
+                    
+                    <div id="header">
+                        <a><i class="fa fa-bookmark" aria-hidden="true"></i></a>
+                        <h3>Life Science Department</h3>
+                    
+                    </div>
+                
+                </div>
+                
+                <div class="col-md-12 seg2">
+                    
+                    <h1>Research and Innovation</h1>
+                
+                
+                
+                </div>
+            
+            
+            
+            </div>
+        
+        
+        
+        </div>
+    
+    
+    
+    
+    
+    
+    </body>
+
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html>
+    
+    <body>
+        
+        <head>
+            
+            <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Advent+Pro|Alfa+Slab+One|Anton|Archivo+Narrow|Baloo+2|Baloo+Chettan+2|Bellota|BenchNine|Caveat|Cinzel|Comfortaa|Concert+One|Cookie|Damion|Dancing+Script|Dosis|Electrolize|Fredoka+One|Great+Vibes|Indie+Flower|Inter|Josefin+Sans|Josefin+Slab|Kaushan+Script|Lato|Lobster|Luckiest+Guy|Monoton|Montserrat|Open+Sans|Oswald|Oxanium|PT+Sans+Caption|Pacifico|Parisienne|Passion+One|Poiret+One|Poppins|Quattrocento+Sans|Raleway|Righteous|Roboto+Mono|Sacramento|Satisfy|Sen|Source+Sans+Pro|Spartan|Titillium+Web|Ubuntu|Ultra|Yellowtail&display=swap" rel="stylesheet">
+            
+            
+            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+            <meta charset="utg-8">
+    
+            <meta name="viewport" width="device-width , initial-scale=1">
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+            <!-- Popper JS -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+           
+           
+            <style type="text/css">
+                
+                .seg1{
+                    height: 120px;
+                    background-color: crimson;
+                    
+                    
+                }
+                
+                .seg1_1{
+                    
+                    background-color: aliceblue;
+                    margin-left: 350px;
+                    margin-top: -10px;
+                }
+                
+                .seg1_1 li{
+                    float: left;
+                    justify-content: flex-end;
+                    list-style-type: none;
+                    
+                    background-color: transparent;
+                    margin-left: 5px;
+                    font-family: Spartan;
+                    text-align: center;
+                    color: gold;
+                }
+                
+                .seg1_1 li:hover{
+                    cursor: pointer;
+                    background:linear-gradient(90deg,gold, orange);
+                    color:crimson;
+                
+                }
+                
+                
+                #logo{
+                    width: 180px;
+                    height: 80px;
+                    background-color: transparent;
+                }
+                
+                #logo p{
+                    font-family:Spartan;
+                    font-size: 30px;
+                    margin-top: 20px;
+                    color: gold;
+                    font-weight: bold;
+                }
+                
+                
+                .seg2{
+                    height:1050px;
+                    background-color: #FAF9F9;
+                }
+                
+                #p1{
+                    width: 560px;
+                    font-family: Poppins;
+                    font-size: 45px;
+                    margin-top: 100px;
+                }
+                
+                .seg2 div{
+                    height: 300px;
+                    background-color: white;
+                    border: solid thin grey;
+                    border-radius: 5px;
+                    box-shadow: 0 0 8px gainsboro;
+                    float: left;
+                    margin-left: 20px;
+                    margin-top: 50px;
+                    padding-top: 10px;
+                }
+                
+                .seg2_2{
+                    background-color:gainsboro;
+                    background-image:url(Rospharmacy-768x342.jpg);
+                    background-size: 450px;
+                    padding-right: 20px;
+                    height: 950px;
+                    
+                }
+                .seg2_2 a{
+                    font-size: 125px;
+                    color: gold;
+                }
+                
+                
+                
+                .department_labels{
+                    
+                    height: 34px;
+                    background:linear-gradient(90deg, gold, orange);
+                    text-align: center;
+                    padding-left: 10px;
+                    padding-top: 5px;
+                    font-family: Poppins;
+                    line-height: -10px;
+                }
+                
+                .department1{
+                    background-image: url(Engineering-EDIWeekly.jpg);
+                    background-size: cover;
+                    overflow: hidden;
+                }
+                .department1:hover{
+                    cursor: pointer;
+                   
+                    overflow: hidden;
+                    
+                    
+                }
+                
+                
+                .department2{
+                    background-image:url(dna-rna.jpg);
+                    background-size: cover;
+                    overflow: hidden;
+                }
+                .department2:hover{
+                    cursor: pointer;
+                }
+                
+                .department3{
+                    background-image: url(physical_science.png);
+                    background-size: cover;
+                    overflow: hidden;
+                }
+                .department3:hover{
+                    cursor: pointer;
+                }
+                
+                
+                .department4{
+                    background-image: url(1512676700-7251.jpg);
+                    background-size: cover;
+                    overflow: hidden;
+                }
+                .department4:hover{
+                    cursor: pointer;
+                }
+                
+                
+                
+                
+                .r2_seg1{
+                    height: 600px;
+                    background-color: gray;
+                    background-image:url(odessa-ukraina-nacionalnyy.jpg);
+                    background-size: cover;
+                    
+                }
+                
+                .r2_seg1_text{
+                    background-color: aliceblue;
+                    height: 250px;
+                    margin-top: 30px;
+                    justify-content:center;
+                    opacity: 0.8;
+                    border-radius: 5px;
+                }
+                .r2_seg1_text h2{
+                    color: darkorange;
+                }
+                
+                
+                
+                .footer{
+                    height: 350px;
+                    background-color: beige;
+                }
+                
+                
+                .footer1_1{
+                    height: 350px;
+                    background-color: beige;
+                    
+                }
+                #social{
+                    width: 180px;
+                    height: 100px;
+                    
+                    margin-left: 130px;
+                    margin-top: 10px;
+                    
+                }
+                #social a {
+                    font-size: 35px;
+                    margin-left: 20px;
+                    color: green;
+                }
+                
+                
+                
+                
+                
+                
+                
+                /*Media Query For Responsiveness */
+                
+                @media screen and (max-width: 768px){
+                    
+                    .seg2 div{
+                        width: "col-md-4";
+                        background-color: aliceblue;
+                    }
+                    
+                    .department_labels{
+                        
+                        
+                    }
+                    #dl1{
+                        
+                    }
+                    
+                }
+                
+              
+
+
+            </style>
+
+
+        <head>
+            
+            
+        <div class="container-fluid">
+            
+            <div class="row">           <!--ROW 1--->
+                <div class="col-md-12 seg1">
+                    
+                    <div id="logo">
+                        <p>LOGO</p>
+                    
+                    </div>
+                    
+                    <ul class="col-md-8 seg1_1" justify-content="flex-end">
+                        
+                            
+                            <li class="col-sm-2">Home</li>
+                            <li class="col-sm-2">Brochure</li>
+                            <li class="col-sm-2">Department</li>
+                            <li class="col-sm-2">About</li>
+                        
+                        
+                    </ul>
+                
+                
+                </div>
+                
+                
+                <div class="col-md-8 seg2">
+                    <blockquote id="p1">International Journal of <strong>AAFT University </strong></blockquote>
+                    
+                    <a href="Engineering.php"><div class="col-md-3 department1">
+                        
+                        <label class="col-md-12 department_labels" id="dl1">Engineering</label>
+                    
+                    </div></a>
+                    <a href="Life_sc.php"><div class="col-md-3 department2">
+                        <label class="col-md-12 department_labels" id="dl2">Life Science</label>
+                    
+                    </div></a>
+                    <a href="Physical_sc.php"><div class="col-md-3 department3">
+                        <label class="col-md-12 department_labels" id="dl3">Science</label>
+                    
+                    </div></a>
+                    <a href="Pharmacy.php"><div class="col-md-3 department4">
+                        <label class="col-md-12 department_labels" id="dl4">Pharmacy</label>
+                        
+                    </div></a>
+                
+                
+                </div>
+            
+                <div class="col-md-4 seg2_2">
+                    
+                    
+                </div>
+            
+            
+            </div>
+            
+            
+            <div class="row">           <!--ROW 2-->
+                
+                <div class="col-md-12 r2_seg1">
+                    
+                    <div class="col-md-4 r2_seg1_text" justify-content="flex-end">
+                        <h2>" Academic Excellence and Performance!"</h2>
+                        
+                    </div>
+                
+                
+                </div>
+            
+                
+            </div>
+            
+            
+            <div class="row">                   <!--ROW 3-->
+                
+                <div class="col-md-8 footer">
+                    <h4>This is a Aaft University Affiliated Unit</h4>
+                    <h6>Disclaimer!</h6>
+                    <p>This is subject to AAFT</p>
+                    
+                </div>
+                
+                <div class="col-md-4 footer1_1">
+                    
+                    <div id="social">
+                        <a href=""><i class="fa fa-twitter" ></i></a>
+                        <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+            
+            
+            
+        </div>    
+        
+            
+
+
+    </body>
+            
+</html>
